b520d6eb4ee44187b07659611a67ed52bf136870
yaneth hernandez <yahecaceres@gmail.com>
2023-09-14 14:53:03 -0500

    Cambios en el app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -13,8 +15,6 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DetailHeroComponent } from './components/detail-hero/detail-hero.component';
-
-
 
 @NgModule({
   declarations: [
